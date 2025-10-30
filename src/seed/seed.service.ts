9957c0a132f12cfcf269eeb8ece56f5c7f7887f7
Jaime Cantais <jaime@heartcoded.es>
2025-10-30 12:06:21 +0100

    Antes de insertar por lote

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+
+@Injectable()
+export class SeedService {
+
+  private readonly axios: AxiosInstance = axios;
+
+  async executeSeed() {
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
+
+    data.results.forEach(async ({ name, url }) => {
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+      console.log({ name, no });
+      // Aquí podrías guardar en la base de datos usando un servicio adecuado
+      // await this.pokemonService.create({ name, no });
+    });
+
+    return data.results;
+  }
+}
